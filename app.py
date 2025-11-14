3cb5ae80e3607fd011b858f9474dd4ca0891f5f3
SandersonTI <sandersonbkp@gmail.com>
2025-11-14 05:50:13 -0300

    Criação do tópico 2
    Foi feito a implementação do  da rota /cadastrar para receber os dados do usuário e  validar o tipo  (Turista, parceiro e administrador, além de armazenar o novo usuário na lista DB_USUARIO.
    Para isso foi necessário substituir o código existente da função  cadastrar() pela atual.
    
    O código no momento do teste apresentou erro por causa da declaração global "global NEXT_USER_ID", primeiro estava no local errado na linha 60 ao retirar rodou porém apresentou erro no arquivo http ao testar.
    O arquivo http apresentou erro no teste e só pegou após inserir ###, pois havia vários JSON seguidos no mesmo request. O REST Client só aceita um único JSON por requisição. Após separar  cada requisição (JSON) por "###" funcionou.
    E só ficou td redondo quando a declaração global "global NEXT_USER_ID" foi inserida no inicio da função "cadastrar()" na linha 27

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 DB_USUARIOS = []
+global NEXT_USER_ID
 NEXT_USER_ID = 1
 
 DB_PUBLICACAO = []
@@ -13,6 +14,8 @@ publicacao_inicial = {
 }
 DB_PUBLICACAO.append(publicacao_inicial)
 
+TIPOS_USUARIO_VALIDOS = ["Turista","Parceiro", "Administrador"]
+
 app = Flask(__name__)
 
 @app.route('/entrar', methods=['POST'])
@@ -21,12 +24,51 @@ def entrar():
 
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
-    return jsonify({"mensagem": "Rota de Cadastro: Implementação pendente no Tópico 2."})
+    global NEXT_USER_ID
+    dados = request.get_json()
+    if not dados:
+        return jsonify({"erro": "Dados ou formato incorreto (JSON)."}), 400
+
+    campos_obrigatorios = ['nome', 'idade', 'telefone', 'email', 'senha', 'tipo_usuario']
+
+    for campo in campos_obrigatorios:
+        if campo not in dados:
+            return jsonify({"erro": f"Campo obrigatório ausente: {campo}."}), 400
+        
+    if not isinstance(dados['idade'], int) or dados['idade'] <= 0:
+        return jsonify({"erro": "Idade deve ser um número inteiro positivo."}), 400
+    
+    for usuario in DB_USUARIOS:
+        if usuario['email'] == dados['email']:
+            return jsonify({"erro": "E-mail já cadastrado. E-mail deve ser único (login)."}), 400
+                            
+    tipo_usuario = dados['tipo_usuario']
+    if tipo_usuario not in TIPOS_USUARIO_VALIDOS:
+        return jsonify({"erro": f"Tipo de usuário inválido. Escolha entre: {', '.join(TIPOS_USUARIO_VALIDOS)} "})
+    
+    novo_usuario = {
+        'id': NEXT_USER_ID,
+        'nome': dados['nome'],
+        'idade': dados['idade'],
+        'telefone': dados['telefone'],
+        'email': dados ['email'],
+        'senha': dados ['senha'],
+        'tipo_usuario': tipo_usuario
+    }
+
+    DB_USUARIOS.append(novo_usuario)
+    
+    NEXT_USER_ID += 1
+
+    usuario_resposta = novo_usuario.copy()
+    del usuario_resposta['senha']
+    return jsonify({
+        "mensagem": "Usuário cadastrado com sucesso!", "usuario": usuario_resposta
+    }), 201
 
 @app.route('/', methods=['GET'])
 def publicacao_home():
     return jsonify(DB_PUBLICACAO[0])
 
 if __name__=='__main__':
-
     app.run(debug=True)
